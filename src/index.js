a4fac69ff291d9c79269ad60818838a8207a87ee
Danil Chemodanov <dan.cmdn@gmail.com>
2020-10-21 23:23:47 +0300

    feat: solve all tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 module.exports = function toReadable(number) {
-	let num = number.toString();
-	for (let i = number.toString().length; i < 3; i++) {
-		num = `-${num}`;
-	}
-
 	let oneToNine = {
+		'0': '',
 		'1': 'one',
 		'2': 'two',
 		'3': 'three',
@@ -15,45 +11,12 @@ module.exports = function toReadable(number) {
 		'8': 'eight',
 		'9': 'nine'
 	};
-	let hundreds = oneToNine.map(element => {
-		element += ' hundreed';
-	})
-	let hundreds['-'] = '';
-	let hundreds = {
-		'-': '',
-		'1': 'one hundred',
-		'2': 'two hundred',
-		'3': 'three hundred',
-		'4': 'four hundred',
-		'5': 'five hundred',
-		'6': 'six hundred',
-		'7': 'seven hundred',
-		'8': 'eight hundred',
-		'9': 'nine hundred'
-	};
-	let firstPos = hundreds[num[0]]
+	function oneToNineCalc(number) {
+		return oneToNine[number.toString()];
+	}
 
-	let tenUnderTwenty = {
-		'-0': 'zero',
-		'-1': 'one',
-		'-2': 'two',
-		'-3': 'three',
-		'-4': 'four',
-		'-5': 'five',
-		'-6': 'six',
-		'-7': 'seven',
-		'-8': 'eight',
-		'-9': 'nine',
-		'01': 'one',
-		'02': 'two',
-		'03': 'three',
-		'04': 'four',
-		'05': 'five',
-		'06': 'six',
-		'07': 'seven',
-		'08': 'eight',
-		'09': 'nine',
-		'10': 'ten'
+	let tenToNineteen = {
+		'10': 'ten',
 		'11': 'eleven',
 		'12': 'twelve',
 		'13': 'thirteen',
@@ -64,9 +27,65 @@ module.exports = function toReadable(number) {
 		'18': 'eighteen',
 		'19': 'nineteen'
 	};
-	let tenAfterTwenty;
+	function tenToNineteenCalc(number) {
+		return tenToNineteen[number.toString()];
+	}
 
-	let secondPos = tenUnderTwenty['' + num[1] + num[2]];
-	let arr = [];
-	return arr.concat(firstPos, secondPos).join(' ');
+	let tenths = {
+		'2': 'twenty',
+		'3': 'thirty',
+		'4': 'forty',
+		'5': 'fifty',
+		'6': 'sixty',
+		'7': 'seventy',
+		'8': 'eighty',
+		'9': 'ninety'
+	};
+	function tenthsCalc(number) {
+		let arr = [];
+		return (tenths[(number.toString())[0]] + ' ' + oneToNineCalc(number.toString()[1])).trim();
+	}
+
+	let hundreds = {
+		'1': 'one hundred',
+		'2': 'two hundred',
+		'3': 'three hundred',
+		'4': 'four hundred',
+		'5': 'five hundred',
+		'6': 'six hundred',
+		'7': 'seven hundred',
+		'8': 'eight hundred',
+		'9': 'nine hundred'
+	};
+	function hundredsCalc(number) {
+		let num = Number(number.toString().slice(1));
+		if (num <= 9) {
+			return (hundreds[(number.toString())[0]] + ' ' + oneToNineCalc(num)).trim();
+		} else if (num <= 19) {
+			return (hundreds[(number.toString())[0]] + ' ' + tenToNineteenCalc(num)).trim();
+		} else if (num >= 20) {
+			return (hundreds[(number.toString())[0]] + ' ' + tenthsCalc(num)).trim();
+		}
+	}
+
+	switch (number.toString().length) {
+		case 1:
+			if (number === 0) {
+				return 'zero'
+			};
+			return oneToNineCalc(number);
+			break;
+		case 2:
+			if (number <= 19) {
+				return tenToNineteenCalc(number);
+			} else if (number >= 20) {
+				return tenthsCalc(number);
+			}
+			break;
+		case 3:
+			return hundredsCalc(number);
+			break;
+		default:
+			break;
+	}
 }
